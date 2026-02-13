1f8ac88057b09ab420bd160a0804feeeca20a257
Saul <123047731@upq.edu.mx>
2026-02-13 13:18:47 -0600

    CRUD
    Se crearon el CRUD para usuarios

---
+++ miAPI/app/main.py
@@ -0,0 +1,110 @@
+#importaciones
+from typing import Optional
+from fastapi import FastAPI,status,HTTPException
+import asyncio
+
+#Iniciaclización o instancia de la API
+app= FastAPI(
+    title='Mi primer API',
+    description='Silva Ongay Saúl',
+    version='1.0'
+)
+
+#BD ficticia
+usuarios=[
+    {"id":1,"nombre":"Saúl","edad":24},
+    {"id":2,"nombre":"Memi","edad":21},
+    {"id":3,"nombre":"Mauchín","edad":48},
+]
+
+
+#Endpoints
+@app.get("/", tags=['Inicio'])
+async def holamundo():
+    return {"mensaje":"Hola mundo FastAPI "}
+
+@app.get("//v1/bienvenidos", tags=['Inicio'])
+async def bienvenido():
+    return {"mensaje":"Bienvenidos a tu API REST "}
+
+@app.get("/v1/calificaciones", tags=['Asincronia'])
+async def calificaciones():
+    await asyncio.sleep(5)
+    return {"mensaje":"calificacion en TAI es 7 "}
+
+@app.get("/v1/parametroO/{id}", tags=['Parametro obligatorio'])
+async def consultaUsuarios(id:int):
+    await asyncio.sleep(3)
+    return {"usuario encontrado":id}
+
+@app.get("/v1/ParametroOp/", tags=['Parametro opcional'])
+async def consultaOp(id: Optional[int]=None):
+    await asyncio.sleep(3)
+    if id is not None:
+        for usuario in usuarios: #Se busca usuario en el diccionario de usuarios
+            if usuario["id"]== id:
+                return { "Usuario encontrado":id,
+                         "Datos": usuario 
+                         }
+            
+        return {"Mensaje":"Usuario no encontrado"}
+    
+    else: 
+        return {"AVISO":"No se proporcionó ID"}
+    
+
+@app.get("/v1/usuarios/", tags=['CRUD usuarios'])
+async def consultaUsuarios():
+    
+    return{
+        "status":"200",
+        "total": len(usuarios),
+        "data":usuarios
+    }
+
+@app.post("/v1/usuarios/", tags=['CRUD usuarios'])
+async def agregar_usuarios(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            raise HTTPException(
+             status_code= 400,
+             detail= "El ID ya existe xd"
+        ) 
+    usuarios.append(usuario)
+    return{
+        "mensaje":"Usuario agregado exitosamente",
+        "datos":usuario,
+        "status":"200"
+    }
+
+@app.put("/v1/usuarios/{id}", tags=['CRUD Usuarios'])
+async def actualizar_usuario(id: int, usuario: dict):
+    for idx, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios[idx] = {**usr, **usuario}
+            return {
+                "mensaje": "Usuario actualizado correctamente xd",
+                "datos": usuarios[idx],
+                "status": "200"
+            }
+            
+    raise HTTPException(
+        status_code=400,
+        detail="Usuario no encontrado"
+        )
+
+@app.delete("/v1/usuarios/{id}", tags=['CRUD Usuarios'])
+async def eliminar_usuario(id: int):
+    for idx, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            del usuarios[idx]
+            return {
+                "mensaje": "Usuario eliminado exitosamente xdd",
+                "status": "200"
+            }
+        raise HTTPException(
+        status_code=400,
+        detail="Usuario no encontrado"
+        )
+
+#Swagger es lo que se utiliza para la documentación automática
